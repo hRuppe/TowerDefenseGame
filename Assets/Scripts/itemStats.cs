b8d111e56ff6dccaed1f1352502c1c26ba422601
Eli Rogers <eli_rogers96@ymail.com>
2024-02-23 02:02:28 -0500

    Added prefabs

---
+++ Assets/Scripts/itemStats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class itemStats : MonoBehaviour
+{
+    public string itemName;
+    public int itemID; 
+    public float healingAmount;
+    public int despawTimer;
+    public int itemCount;
+    public GameObject itemModel;
+    public GameObject itemEffect;
+    public AudioClip itemSound;
+}
